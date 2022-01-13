7b2342460a2bf1ccd84a715c6611543b70fceb83
Euris Aquino <euris051413@gmail.com>
2022-01-12 22:29:17 -0400

    5to Commit

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using my_books.Data.Models;
 
 namespace my_books.Data
 {
-    public class AppDbContext
+    public class AppDbContext:DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Book> Books { get; set; }
     }
 }
